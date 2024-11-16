800b30f7f2cca49733e4b71ccf870d4bd1fc3348
Nadia Adriano <nadiaadriano49@gmail.com>
2024-11-16 14:44:32 -0800

    Input box clears when enter clicked

--- frontend/script.js
+++ frontend/script.js
@@ -27,8 +27,29 @@ document.getElementById('send-button').addEventListener('click',() => {
         console.log('Message sent successfully:', data);
         let chat= document.getElementById("chat-container")
         chat.innerHTML = chat.innerHTML+`<div class="bubble left">${data.gusReply}</div>`
+
+        //Clear the message box
+        msgbox.value = '';
     })
     .catch(error => {
         console.error('Error sending message:', error);
     });
 });
+
+//Add event listener for the Enter key
+msgbox.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Prevent the default action (like form submission)
+        document.getElementById('send-button'),click(); // Simulate a click on the send button
+ 
+    }
+});
+    
+
+
+
+
+
+
+
+
