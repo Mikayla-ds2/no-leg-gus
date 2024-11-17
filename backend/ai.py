4a4dc0b7f14ae96257a25a803d1a27968d8f96ab
Mikayla Solomon <mikaylasolomon68@gmail.com>
2024-11-16 21:38:06 -0800

    finished the akinator logic

--- backend/ai.py
+++ backend/ai.py
@@ -14,56 +14,62 @@ d = {
     'Neptune' : [-330, 15299, 57754, 16]
 }
 
-remaining_planets = list(d.keys())
-question = f'Is the mean temperature of the planet more than 0 degrees Fahrenheit?'
-print(question)
-response = input('(yes/no): ').strip.lower()
-if response == 'yes':
-    remaining_planets.remove('Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune')
-    print(remaining_planets)
-    question2 = f'Is the mean radius of the planet more than 10,000 miles?'
-    print(question2)
-    if response == 'yes':
-        
-        
-elif response == 'no': 
-    remaining_planets.remove('Earth', 'Mercury', 'Venus')
-    print(remaining_planets)
-
-        
-    
-    
-    
-    
-    
-'''
-    if characteristic == 'temp':
-        question = f'Is the mean temperature of the planet {>} {0} degrees Fahrenheit?'
-    elif characteristic == 'radius':
-        question = f'Is the mean radius of the planet {>} {10,000} miles?'
-    elif characteristic == 'volume':
-        question = f'Is the mean volume of the planet {>} {50,000} billion cubic miles?'
-    elif characteristic == 'number of moons': 
-        question = f'Is the number of moons the planet has {<} {10}?'
+def akinator_planet_style():
+    remaining_planets = list(d.keys())
+    question = f'Is the mean temperature of the planet more than 0 degrees Fahrenheit?'
     print(question)
-    response = input('yes/no): ').strip.lower()
-    return response == 'yes'
-def filter_planets(remaining_planets, characteristic, comparison, value): 
-    new_planets = []
-    for planet in remaining_planets: 
-        if comparison == ">":
-            if d[planet][characteristic_index(characteristic)] > value:
-                new_planets.append(planet)
-    return new_planets
-while len(remaining_planets) > 1: 
-    if ask_questions(remaining_planets, "radius", ">", )
-'''   
-
-
-
-'''
-Is the mean temperature of the planet more than or less than 0 degrees Fahrenheit?
-Is the mean radius of the planet more than or less than 10,000 miles?
-Is the mean volume of the planet more than or less than 50,000 billion cubic miles?
-Is the number of moons the planet has more than or less than 10?
-'''
+    response = input('(yes/no): ').strip.lower()
+    if response == 'yes':
+        remaining_planets.remove('Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune')
+        print(remaining_planets)
+        question2 = f'Is the mean volume of the planet more than 100 billion cubic miles?'
+        print(question2)
+        response = input('(yes/no): ').strip.lower()
+        if response == 'yes': 
+            remaining_planets.remove('Mercury')
+            question3 = f'Is the number of moons the planet has 0?'
+            print(question3)
+            response = input('(yes/no): ').strip.lower()
+            if response == 'yes': 
+                remaining_planets.remove('Earth')
+                return remaining_planets
+            elif response == 'no':
+                remaining_planets.remove('Venus')
+                return remaining_planets
+        elif response == 'no': 
+            remaining_planets.remove('Earth', 'Venus')
+            return remaining_planets
+    elif response == 'no': 
+        remaining_planets.remove('Earth', 'Mercury', 'Venus')
+        print(remaining_planets)
+        question2 = f'Is the mean radius of the planet more than 20,000 miles?'
+        print(question2)
+        response = input('(yes/no): ').strip.lower()
+        if response == 'yes': 
+            remaining_planets.remove('Mars', 'Uranus', 'Neptune')
+            question3 = f'Is the number of moons the planet has more than 100?'
+            response = input('(yes/no): ').strip.lower()
+            if response == 'yes': 
+                remaining_planets.remove('Jupiter')
+                return remaining_planets
+            elif response == 'no': 
+                remaining_planets.remove('Saturn')
+                return remaining_planets
+        elif response == 'no': 
+            remaining_planets.remove('Jupiter', 'Saturn') 
+            question3 = f'Is the mean volume of the planet more than 50 billion cubic miles?'
+            print(question3)
+            response = input('(yes/no): ').strip.lower()
+            if response == 'yes': 
+                remaining_planets.remove('Mars')
+                question4 = f'Is the number of moons more than 20?'
+                response = input('(yes/no): ').strip.lower()
+                if response == 'yes': 
+                    remaining_planets.remove('Neptune')
+                    return remaining_planets
+                elif response == 'no':
+                    remaining_planets.remove('Uranus')
+                    return remaining_planets
+            elif response == 'no':
+                remaining_planets.remove('Neptune', 'Uranus')
+                return remaining_planets
